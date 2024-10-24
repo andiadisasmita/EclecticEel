884d95d6cda5baeb5dd7bbe4aa7b452232d30302
andiadisasmita <andiadisasmita@gmail.com>
2024-10-24 18:55:46 +0700

    [milestone project phase 0] Adding animation to index & about us

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-// logo-home animation
+// Home logo animation
 document.addEventListener("DOMContentLoaded", function() {
     const logo = document.getElementById("logo-home");
 });
+
+// About Us Slideshow
+const slides = document.querySelectorAll('.slideshow');
+let currentSlide = 0;
+
+function showNextSlide() {
+    // Hide the current slide
+    slides[currentSlide].style.display = 'none';
+    // Move to the next slide
+    currentSlide = (currentSlide + 1) % slides.length;
+    // Show the next slide
+    slides[currentSlide].style.display = 'block';
+}
+
+// Change the slide every 1.7 seconds
+setInterval(showNextSlide, 1700);
